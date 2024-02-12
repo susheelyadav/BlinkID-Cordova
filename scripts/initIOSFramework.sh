0c631b3cbc88813679c8397b34d4303bf7f0052c
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2024-02-12 19:27:10 +0530

    ss

---
+++ scripts/initIOSFramework.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# enter into ios project folder
+HERE="$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")"
+pushd "${HERE}/../src/ios/" > /dev/null
+
+LINK='https://github.com/BlinkID/blinkid-ios/releases/download/v6.4.0/BlinkID.xcframework.zip'
+FILENAME='BlinkID.xcframework.zip'
+
+# BlinkID framework will be obtained via wget or curl
+if which wget >/dev/null ; then
+    echo "Downloading BlinkID framework via wget:"
+    wget -O "${FILENAME}" "${LINK}" -nv --show-progress || ( echo "ERROR: couldn't download BlinkID framework, something went wrong while downloading framework from ${LINK}" && exit 1 )
+elif which curl >/dev/null ; then
+    echo "Downloading BlinkID framework via curl:"
+    curl -o "${FILENAME}" -L "${LINK}" --progress-bar --show-error || ( echo "ERROR: couldn't download BlinkID framework, something went wrong while downloading framework from ${LINK}" && exit 1 )
+else
+    echo "Couldn't download BlinkID framework, neither wget nor curl is available."
+fi
+
+if [ -d 'BlinkID.bundle' ] ; then
+    rm -rf BlinkID.bundle && echo "Removing BlinkID.bundle"
+fi
+
+if [ -d 'BlinkID.framework' ] ; then
+    rm -rf BlinkID.framework && echo "Removing BlinkID.framework"
+fi 
+
+
+if [ -d 'BlinkID.xcframework' ] ; then
+    rm -rf BlinkID.xcframework && echo "Removing BlinkID.xcframework"
+fi 
+
+echo "Unzipping ${FILENAME}"
+unzip -v > /dev/null 2>&1 || { echo "ERROR: couldn't unzip BlinkID xcframework, install unzip" && exit 1; }
+unzip -o "${FILENAME}" > /dev/null 2>&1 && echo "Unzipped ${FILENAME}"
+
+echo "Removing unnecessary files"
+
+rm -rfv BlinkID.xcframework.zip >/dev/null 2>&1
+rm "${FILENAME}" >/dev/null 2>&1
+
+popd
---
+++ src/ios/sources/MBSerializationUtils.h
@@ -0,0 +1,21 @@
+//
+//  MBSerializationUtils.h
+//  BlinkIdDevDemo
+//
+//  Created by DoDo on 01/06/2018.
+//
+
+#import <BlinkID/BlinkID.h>
+#import <Foundation/Foundation.h>
+
+@interface MBSerializationUtils : NSObject
+
++(NSDictionary * _Nonnull) serializeNSDate:(NSDate * _Nullable) value;
++(NSDictionary * _Nonnull) serializeMBDateResult:(MBDateResult * _Nullable) value;
++(NSDictionary * _Nonnull) serializeMBStringResult:(MBStringResult * _Nullable) value;
++(NSNumber * _Nullable)serializeMBSide:(MBSide) value;
++(NSString * _Nullable) encodeMBImage:(MBImage * _Nullable) image;
++(NSDictionary * _Nonnull)serializeCGPoint:(CGPoint) point;
++(NSDictionary * _Nonnull) serializeMBQuadrangle:(MBQuadrangle * _Nonnull) quad;
++(NSDictionary * _Nonnull)serializeCGRect:(CGRect) rect;
+@end
